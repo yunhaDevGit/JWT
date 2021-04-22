f5a243bda6123b2205d96f6d2c58d0be76a48b87
yunhaDev <yunhaDev98@gmail.com>
2021-04-22 18:25:09 +0900

    docs: add description
    - spring security config when use jwt

--- src/main/java/com/practice/jwt/config/SecurityConfig.java
+++ src/main/java/com/practice/jwt/config/SecurityConfig.java
@@ -18,7 +18,7 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
+@RequiredArgsConstructor //
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   private final CorsFilter corsFilter;
@@ -36,12 +36,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     http.csrf().disable();
 
-    // 세션을 사용하지 않겠다
+    // 세션을 사용하지 않겠다 -> stateless 서버로 만들겠다
+    // (기본적으로 웹은 stateless인데 그걸 statefull처럼 사용하기 위해 session과 cookie를 사용한다 하지만 그 방식을 사용하지 않겠다는 뜻)
     http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
-        .addFilter(corsFilter) // 인증 있을 때 security filter 등록!!
-        .formLogin().disable()
-        .httpBasic().disable()
+        .addFilter(corsFilter) // 인증 있을 때 security filter에 등록!! (cross origin 정책 사용 x - 모든 요청 허용)
+        .formLogin().disable() // formLogin 안쓰겠다 (jwt 방식으로 로그인 하기 때문에 기존에 id, pw 쓸 필요 x)
+        .httpBasic().disable() // 기존의 하던 방식과는 다른 방식
         .addFilter(new JwtAuthenticationFilter(authenticationManager()))
         .addFilter(new JwtAuthorizationFilter(authenticationManager(), userRepository))
         .authorizeRequests()
@@ -51,7 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
         .antMatchers("/api/v1/admin/**")
         .access("hasRole('ROLE_ADMIN')")
-        .anyRequest().permitAll();
+        .anyRequest().permitAll(); // 다른 요청은 전부 허용(권한 없이 접근 가능)
   }
 
 }
